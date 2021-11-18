55a5a657f5c6c854ed6559d69a34e9abf9c0fdf0
GenomeOrganizer <californiasteven@outlook.com>
2021-11-18 11:27:13 -0800

    updated

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os
 import setuptools.command.build_py
 from setuptools import setup, find_packages
 from setuptools.command.install import install as _install
@@ -7,6 +8,12 @@ class Install(_install):
     def run(self):
         _install.do_egg_install(self)
         #nothing else to do
+        print("Running setup")
+        try:
+            os.system("cp pymail/pymail /usr/local/bin")
+        except:
+            print("Unable to copy to /usr/local/bin -- recommended that you copy the script to this location")
+
 
 setup(
     cmdclass={
@@ -17,7 +24,7 @@ setup(
     url='https://github.com/genomeorganizer/pymail.git',
     # git+https://github.com/sequencecentral/twitter-bot.git@main#egg=twitterbot
     author='Steve Ayers, Ph.D.',
-    author_email='steve@sequenccecentral.com',
+    author_email='steve@sequencecentral.com',
     # install_requires=[],
     version='1.0.0',
     license='MIT',
---
+++ setup.sh
@@ -0,0 +1,2 @@
+#! /usr/bin/bash
+cp pymail/pymail /usr/local/bin
